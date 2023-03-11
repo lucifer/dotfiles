36d4b3764dddbf63e934280575a8dc52d04d4c38
lucifer <lucifer.stevenson@gmail.com>
2023-03-11 03:16:43 +0000

    Switch to fish before fisher install

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get -y upgrade fish
+/usr/bin/fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
