383b849eb8b590dd28b1c52bf0333efbd21858ca
Lucifer <lucifer.stevenson@gmail.com>
2023-03-11 08:00:29 -0500

    Add fish to allowed shells

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get -y upgrade fish
-/usr/bin/fish
+chsh -s /usr/bin/fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
