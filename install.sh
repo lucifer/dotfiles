f61d7a6ef7fcf43c9363b60cde9af87eb3e8cd2a
Lucifer <lucifer.stevenson@gmail.com>
2023-03-10 20:10:18 -0500

    Add update and fisher plugin manager

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get upgrade -y fish
+sudo apt-get -y update
+sudo apt-get install -y fish fisher
 sudo chsh -s /usr/bin/fish
 
 
