1a8e345dda5cdfb18e387f9ccc6317e2b5cf5d53
Lucifer <lucifer.stevenson@gmail.com>
2023-03-12 10:17:53 -0400

    Remove apt-get upgrade

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo /usr/bin/fish | sudo tee -a /etc/shells
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update
-sudo apt-get -y upgrade fish
+sudo apt-get update && sudo apt-get install -y fish
 exec fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
