47c63467916a46f0daef96e43788b263c8638a97
lucifer <lucifer.stevenson@gmail.com>
2023-03-12 11:21:44 -0400

    Remove exec fish

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@ sudo apt-get remove -y fish && sudo apt-get -y autoremove
 echo/usr/bin/fish | sudo tee -a /etc/shells
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt-get update && sudo apt-get install -y fish
-exec fish
