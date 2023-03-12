4bd76441692e3632bd0be42df4380befb61fb831
lucifer <lucifer.stevenson@gmail.com>
2023-03-12 10:37:14 -0400

    Figure out manually why apt install -y fish gets stuck

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo /usr/bin/fish | sudo tee -a /etc/shells
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install -y fish
+#echo /usr/bin/fish | sudo tee -a /etc/shells
+#sudo apt-add-repository ppa:fish-shell/release-3
+#sudo apt update
+#sudo apt install -y fish
 exec fish
