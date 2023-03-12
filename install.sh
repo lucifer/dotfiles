2f66edb2c5b9f8c12ac026c7cbb69e04d92283d5
lucifer <lucifer.stevenson@gmail.com>
2023-03-12 10:29:11 -0400

    Trying apt over apt-get

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo /usr/bin/fish | sudo tee -a /etc/shells
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update && sudo apt-get install -y fish
+sudo apt update
+sudo apt install fish
 exec fish
