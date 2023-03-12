187650c655832d94833017694d01a440fecf1d5b
lucifer <lucifer.stevenson@gmail.com>
2023-03-12 11:42:56 -0400

    Remove fish install, trying devcontainer template

--- install.sh
+++ install.sh
@@ -1,5 +1,1 @@
 #!/usr/bin/env bash
-sudo apt-get remove -y fish && sudo apt-get -y autoremove
-echo/usr/bin/fish | sudo tee -a /etc/shells
-sudo apt-add-repository -y ppa:fish-shell/release-3
-sudo apt-get update && sudo apt-get install -y fish
