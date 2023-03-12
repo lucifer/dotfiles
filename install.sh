52cf42a59ce3621f708ed4e8e1bc63cf84d20e0c
lucifer <lucifer.stevenson@gmail.com>
2023-03-12 10:23:19 -0400

    Remove fisher plugin install

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@ echo /usr/bin/fish | sudo tee -a /etc/shells
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update && sudo apt-get install -y fish
 exec fish
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
